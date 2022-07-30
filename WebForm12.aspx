14a6ef1f53f16ab57908892332d3a4d71a2b24d9
Your Name <09taumang@gmail.com>
2022-07-30 02:17:55 +0200

    Fix some minor errors

--- WebForm12.aspx
+++ WebForm12.aspx
@@ -8,7 +8,31 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+
+
+        <div class="conainter" style="background-image:linear-gradient(180deg, #e6c63b, #030202);
+
+                                       position:absolute;
+                                       top:50%;
+                                       left:50%;
+                                       transform:translate(-50%,-50%); height: 309px;">
+
+            <asp:TextBox ID="txtCode" runat="server" Enabled="False"></asp:TextBox>
+
+            <asp:Button ID="btnGenerate" runat="server" Text="Generate QR Code" OnClick="btnGenerate_Click"/>
+            <asp:Button ID="btnGenNumber" runat="server" Text="Generate Number" OnClick="btnGenNumber_Click"/>
+            <hr/>
+            <asp:image ID="imgQRCode" with="100px" height="210px" runat="server" Visible="false" Width="357px" style="align-items:center;" />
+            <br/>
+            <asp:Button ID="btnRead" runat="server" Text="Read QR Image" OnClick="btnRead_Click"/>
+
+            <asp:Button ID="btnWriting" runat="server" Text="Read text" OnClick="btnWriting_Click"/>
+            <br />
+            <asp:Label ID="LbCode" runat="server" ></asp:Label>
+            <br/>
+            <br/>
+            <asp:Label ID="lblQRCode" runat="server"></asp:Label> 
+        
         </div>
     </form>
 </body>
--- WebForm12.aspx.cs
+++ WebForm12.aspx.cs
@@ -4,14 +4,114 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using ZXing;
 
 namespace WebApplication122
 {
     public partial class WebForm12 : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        protected void Page_Load(object sender, EventArgs e){
 
+        }
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            GenerateCode(txtCode.Text);
+        }
+        //The read Button:
+        protected void btnRead_Click(object sender, EventArgs e)
+        {
+            ReadQRCode();
+        }
+
+        protected void btnGenNumber_Click(object sender, EventArgs e)
+        {
+            GenerateCode2();
+        }
+
+        protected void btnWriting_Click(object sender, EventArgs e)
+        {
+            //create the file stream first, then write into it.
+            SaveCreateFileStream();
+
+
+        }
+
+
+
+        private void SaveCreateFileStream()
+        {
+            //declearation of the file/folder being created:
+            //STEP 1:
+            //Can possibly add the filestream here along with the writing it to the file which is created.
+            //Also add a if statement, to display as to whether the text has been written to the file.
+            var newDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "DataSaved");
+            //There is an exception being called in the code below:
+            Directory.CreateDirectory(newDirectoryPath);
+
+            var newFilePath = Path.Combine(newDirectoryPath, "numbers_assigned.txt");
+
+            var fileStream = File.Create(newFilePath);
+
+            var sw = new StreamWriter(fileStream);
+
+            sw.WriteLine(lblQRCode.Text);
+
+
+        }
+
+        private void GenerateCode2() {
+            Random rand = new Random();
+            long randNum = (long)(rand.NextDouble() * 90000000000) + 100000000;
+            txtCode.Text = randNum.ToString();
+        }
+
+        private void GenerateCode(string name) {
+
+            var Bwriter = new BarcodeWriter();
+            Bwriter.Format = BarcodeFormat.QR_CODE;
+
+            var result = Bwriter.Write(name);
+            string path = Server.MapPath("~/images/QRImage.jpg");
+            var barcodeBitmap = new Bitmap(result);
+
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    barcodeBitmap.Save(memory, ImageFormat.Jpeg);
+                    byte[] bytes = memory.ToArray();
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+            }
+
+            imgQRCode.Visible = true;
+            imgQRCode.ImageUrl = "~/images/QRImage.jpg";
+
+        }
+
+        private void ReadQRCode()
+        {
+            //Reading of the generated barcode
+            var reader = new BarcodeReader();
+            string filename = Path.Combine(Request.MapPath("~/images"), "QRImage.jpg");
+
+            //Detech and decode the barcode inside the bitmap
+            var result = reader.Decode(new Bitmap(filename));
+
+
+
+            if (result != null)
+            {
+                lblQRCode.Text = $"QR code:{result.Text}";
+            }
+
+
+            //STEP 2:
+            //
         }
     }
 }
